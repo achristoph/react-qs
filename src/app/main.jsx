d2e9c7869b712cc112a204054589a160dc6095e3
Arthur Christoph <achristoph@gmail.com>
2017-03-18 20:15:35 -0500

    init commit

---
+++ src/app/main.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                asdf
+            </div>
+        );
+    }
+}
+
+render(<App />, document.getElementById('app'));
